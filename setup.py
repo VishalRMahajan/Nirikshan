f74d126282d361cd6339b56ce989c76ccf0e1d26
Vishal Mahajan <vism06@gmail.com>
2025-03-08 01:17:35 +0530

    feat: Add requirements and setup configuration

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Nirikshan',
+    version= '0.0.0',
+    author= 'Vishal Rajesh Mahajan',
+    author_email= 'vism06@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
